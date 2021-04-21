5988c39286c042f71438455b58d33a8c2f1dbe2a
Luis Ferraz <luisprto@gmail.com>
2021-04-21 17:47:34 -0300

    Final da aula 3 - Users/Message/Busca de mensagens

---
+++ src/services/UsersService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UsersService {
+  private usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+  async create(email: string) {
+    //Verificar se o usuário existe
+    const usersExists = await this.usersRepository.findOne({
+      email,
+    });
+
+    //Se o usuário existe, retorna o mesmo
+    if (usersExists) {
+      return usersExists;
+    }
+
+    //Caso contrario, cria o usuário no DB
+    const user = this.usersRepository.create({
+      email,
+    });
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { UsersService };
